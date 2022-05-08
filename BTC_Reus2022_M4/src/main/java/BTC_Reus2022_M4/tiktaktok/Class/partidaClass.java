31db840e4da9d1f18d811f46f852f65ce1072c98
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-08 19:40:07 +0200

    Commit con char a lista correccion

--- BTC_Reus2022_M4/src/main/java/BTC_Reus2022_M4/tiktaktok/Class/partidaClass.java
+++ BTC_Reus2022_M4/src/main/java/BTC_Reus2022_M4/tiktaktok/Class/partidaClass.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author Josep Oriol L�pez Bosch
  * @author David Dalmau Dieguez
@@ -13,69 +15,82 @@ import java.util.List;
  * @date 05/05/2022
  */
 
-public class partidaClass {
-	private char c1 = 'V', c2 = 'V', c3 = 'V' ,c4 = 'V' ,c5 = 'V', c6 = 'V', c7 = 'V', c8 = 'V', c9 = 'V';	
+public class partidaClass {	
+	// Creamos variables
+	public static char c1 = 'V', c2 = 'V', c3 = 'V' ,c4 = 'V' ,c5 = 'V', c6 = 'V', c7 = 'V', c8 = 'V', c9 = 'V';	
 	private int contadorTurnos = 0;
 	private int turno = 0; 
 	private int ganador = 0;
 
-	private void turnosPartida() {
-		
-	}	
 	
-	// Funcion para el boton de reiniciar
-	private void reiniciarPartida() {
+	// Funcion para asignar casillas
+	public static boolean asignarValoresAChars(List <CasillaClass> listCasillas){
+		boolean flag = false;
+		
+		c1=listCasillas.get(0).getMarcadoCon();
+		c2=listCasillas.get(1).getMarcadoCon();
+		c3=listCasillas.get(2).getMarcadoCon();
+		c4=listCasillas.get(3).getMarcadoCon();
+		c5=listCasillas.get(4).getMarcadoCon();
+		c6=listCasillas.get(5).getMarcadoCon();
+		c7=listCasillas.get(6).getMarcadoCon();
+		c8=listCasillas.get(7).getMarcadoCon();
+		c9=listCasillas.get(8).getMarcadoCon();
 
-	}
-
-	// Funcion para contar las fichas de cada jugador
-	private void comprobarCantidadFichas() {
+		// Comprobamos ganador
+		if (!comprobarGanador().equals("")) {
+			JOptionPane.showMessageDialog(null, "Gana: "+ comprobarGanador());
+			
+			flag = true;
+		}
+		return flag;
 
 	}
 
 	// Funcion para ver quien a ganado
-	public char comprobarGanador() {
-		char ganador = 'A';
+	public static String comprobarGanador() {
+		String ganador = "";
 		// COMPROBAR GANADOR X
 		if ((c1 == ('X')) && (c2 == ('X')) && (c3 == ('X'))) /* HORIZONTAL*/{
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c4 == ('X')) && (c5 == ('X')) && (c6 == ('X'))) {
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c7 == ('X')) && (c8 == ('X')) && (c9 == ('X'))) {			
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c1 == ('X')) && (c4 == ('X')) && (c7 == ('X'))) /* VERTICAL*/{
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c2 == ('X')) && (c5 == ('X')) && (c8 == ('X'))) {
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c3 == ('X')) && (c6 == ('X')) && (c9 == ('X'))) {
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c1 == ('X')) && (c5 == ('X')) && (c9 == ('X'))) /* DIAGONAL*/{
-			ganador = 'X';
+			ganador = "Jugador 1";
 		} else if ((c3 == ('X')) && (c5 == ('X')) && (c7 == ('X'))) {
-			ganador = 'X';
+			ganador = "Jugador 1";
 		}
 
 		// COMPROBAR GANADOR Y
 		if ((c1 == ('Y')) && (c2 == ('Y')) && (c3 == ('Y'))) /* HORIZONTAL*/{
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c4 == ('Y')) && (c5 == ('Y')) && (c6 == ('Y'))) {
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c7 == ('Y')) && (c8 == ('Y')) && (c9 == ('Y'))) {			
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c1 == ('Y')) && (c4 == ('Y')) && (c7 == ('Y'))) /* VERTICAL*/{
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c2 == ('Y')) && (c5 == ('Y')) && (c8 == ('Y'))) {
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c3 == ('Y')) && (c6 == ('Y')) && (c9 == ('Y'))) {
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c1 == ('Y')) && (c5 == ('Y')) && (c9 == ('Y'))) /* DIAGONAL*/{
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		} else if ((c3 == ('Y')) && (c5 == ('Y')) && (c7 == ('Y'))) {
-			ganador = 'Y';
+			ganador = "Jugador 2";
 		}
 		return ganador;
 	}
 
+	// Creamos Getters y Setters
 	public int getContadorTurnos() {
 		return contadorTurnos;
 	}
@@ -99,9 +114,4 @@ public class partidaClass {
 	public void setGanador(int ganador) {
 		this.ganador = ganador;
 	}
-
-	
-	
 }
-
-
